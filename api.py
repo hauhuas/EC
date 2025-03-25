7b6024c3d9d854dc6929732c9301cdb582aacc66
你的名字 <2393563273@qq.com>
2025-03-25 17:35:38 +0800

    创建ECapi

---
+++ api.py
@@ -0,0 +1,19 @@
+import requests
+
+
+
+def check():
+    url = "http://master.versionoms.yunwms.com/default/svc/web-service"
+
+    payload = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"http://www.example.org/Ec/\">\n    <SOAP-ENV:Body>\n        <ns1:callService>\n            <paramsJson>\n                {\n                    \"pageSize\":\"2\",\n                    \"page\":1\n                }\n            </paramsJson>\n            <appToken>cbd534ca9f15aa992b9f6f1bc0296613</appToken>\n            <appKey>781133391fe6a3947617e4056b1ba54c</appKey>\n            <service>getWarehouse</service>\n        </ns1:callService>\n    </SOAP-ENV:Body>\n</SOAP-ENV:Envelope>"
+    headers = {
+      'Content-Type': 'text/xml; charset=utf-8',
+      'SOAPAction': '"callService"',
+      'Cookie': 'PHPSESSID=66dmi38cti8mt5hioeu9i7srms'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    check()
